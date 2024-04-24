5fcd9c306ce212e305e70dc6d974d2b27dabd690
Juan Moreno <juanpablomoreno.r@gmail.com>
2024-04-24 11:26:36 -0400

    sesion 02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import Dexie from "dexie";
+import { useLiveQuery } from "dexie-react-hooks";
+
+import "./App.css";
+import "bulma/css/bulma.css";
+import { FaCheck } from "react-icons/fa";
+import { ImCheckboxUnchecked } from "react-icons/im";
+import { FaRegTrashAlt } from "react-icons/fa";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //Create Database and it's version
+    const db = new Dexie("todoApp");
+    db.version(1).stores({
+        todos: "++id,task,completed"
+    })
+
+
+
+    //Get a view of all tasks in the DB
+    const allTasks = useLiveQuery(() => db.todos.toArray(), []);
+    if (!allTasks) return null
+
+    //Add Task
+    /*
+    First we need to establish an asyncronous function that can make queries on the DB
+    Every query needs to be async
+    */
+    const addTask = async (event) => {
+        event.preventDefault();
+        const taskField = document.getElementById('taskInput');
+
+        await db.todos.add({
+            task: taskField.value,
+            completed: false
+        })
+
+        taskField.value = ''
+    }
+
+    const deleteTask = async(id) => db.todos.delete(id);
+    
+    const toggleStatus = async(id, event) => {
+        await db.todos.update(id, {completed: !!event.target.checked})
+    }
+
+
+
+    return (
+        <div className="container">
+            <form className="section userForm is-flex is-flex-direction-column is-align-items-center" onSubmit={addTask}>
+                <h1 className="title">To Do App</h1>
+                <input id="taskInput" className="input" type="string" placeholder="What do you want to do today?" required />
+                <button type="submit" className="button is-primary is-align-self-flex-end mt-2">Add</button>
+            </form>
+            <div className="section taskList">
+                { allTasks?.map(item => (
+                    <div>
+                        <div className="block taskContainer">
+                            <input type="checkbox" checked={item.completed} className="checkbox" onChange={(event)=>toggleStatus(item.id,event)}></input>
+                            <span className={`task ${item.completed && 'strike-text'}`}>{item.task}</span>
+                            <span className="delete" onClick={()=>deleteTask(item.id)}><FaRegTrashAlt /></span>
+                        </div>
+                        <hr/>
+
+                    </div>
+
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
